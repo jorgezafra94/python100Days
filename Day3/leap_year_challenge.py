7a92d2837c8a6a10d1e8602cfe6e9469a86e9c79
jorge <jorgezafra94@hotmail.com>
2023-12-06 16:13:01 -0600

    improvements

--- Day3/leap_year_challenge.py
+++ Day3/leap_year_challenge.py
@@ -2,10 +2,10 @@
 year = int(input("Which year do you want to check? "))
 
 if year % 4 != 0:
-    print("Not leap year.")
-elif year % 100 != 0 and year % 400 != 0:
-    print("Not leap year.")
+  print("Not leap year")
+elif year % 100 == 0 and year % 400 != 0:
+  print("Not leap year")
 elif year % 100 == 0 and year % 400 == 0:
-    print("Leap year.")
+  print("Leap year")
 else:
-    print("Leap year.")
+  print("Leap year")
